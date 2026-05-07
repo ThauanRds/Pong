7ec24954d0f33cb2f4a0ec3495a476b22012c522
Thauan Rodrigues da Silva <thaudogamedev@gmail.com>
2026-05-07 01:08:37 -0300

    Adição dos efeitos visuais e implementação do segundo jogador

--- Pong/Assets/Scripts/Ball.cs
+++ Pong/Assets/Scripts/Ball.cs
@@ -5,6 +5,7 @@ using DG.Tweening;
 
 public class Ball : MonoBehaviour
 {
+    public GameObject explosion;
     public float speed;
     public TrailRenderer trail;
 
@@ -41,6 +42,10 @@ public class Ball : MonoBehaviour
 
     public void ResetBall()
     {
+        GameObject exp = Instantiate(explosion, transform.position, transform.rotation);
+        Destroy(exp, 2f);
+
+        sprite.transform.localScale = Vector3.one;
         rb.velocity = Vector2.zero;
         trail.Clear();
         trail.enabled = false;
@@ -83,8 +88,12 @@ public class Ball : MonoBehaviour
 
     void BallJuice()
     {
+        // Limpando o DOTween para evitar conflitos de animação
+        sprite.transform.DOKill();
+        sprite.transform.localScale = Vector3.one;
         sprite.transform.DOScale(1.8f, 0.1f).SetLoops(2, LoopType.Yoyo);
 
+        Camera.main.transform.DOKill();
         Camera.main.transform.DOShakePosition(
             0.1f, //Duração
             0.2f, // Força do shake
--- Pong/Assets/Scripts/GameManager.cs
+++ Pong/Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using DG.Tweening;
 
 public class GameManager : MonoBehaviour
 {
@@ -13,13 +14,26 @@ public class GameManager : MonoBehaviour
     
     public void PlayerPoint()
     {
+        JuiceText(playerScoreText);
+
         playerScore++;
         playerScoreText.text = playerScore.ToString();
     }
 
     public void AIPoint()
     {
+        JuiceText(AIScoreText);
+
         AIScore++;
         AIScoreText.text = AIScore.ToString();
+    }
+
+    void JuiceText(TextMeshProUGUI text)
+    {
+        text.transform.DOKill();
+        text.transform.localScale = Vector3.one;
+        text.transform.DOScale(1.5f, 0.5f).SetLoops(2, LoopType.Yoyo);
+
+        //text.DOColor(Color.yellow, 0.5f).SetLoops(2, LoopType.Yoyo);
     }
 }
